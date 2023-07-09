c9c3dc5827ca73a698bec11a85a4389e7d8057d6
Piyush096 <mishra.piyush1886@gmail.com>
2023-07-09 21:21:01 +0530

    update

---
+++ A_04_Operators_Expression.js
@@ -0,0 +1,22 @@
+//  in javascript ** this operator means pwer ex - X ** Y means x to the power y.
+
+
+// compariosion operator:-
+
+ let comp1 = 6;
+ let comp2 = '6';
+
+ console.log("comp == comp2 is",comp1 == comp2);
+ console.log("comp != comp2 is",comp1 != comp2);
+ console.log("comp === comp2 is",comp1 === comp2); // it compares its values and also its type
+ console.log("comp !== comp2 is",comp1 !== comp2); // it compares not equal value and not equal type
+
+
+ // logical operator:-
+// three logical operator logical and, logical or, logical not
+
+let a = 6;
+let b = 5;
+
+console.log(a == b || a > b); // in this way we can use the operators
+
